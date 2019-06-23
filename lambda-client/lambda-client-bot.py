91e7e11b710cd0a96a28c19e0332e149af294123
wass80 <wasss80@gmail.com>
2019-06-23 15:55:49 +0900

    auto miner

--- lambda-client/lambda-client-bot.py
+++ lambda-client/lambda-client-bot.py
@@ -44,6 +44,57 @@ def post(msg):
       'icon_emoji': u':bitcoin:',
   }))
 
+def jsonloads(i):
+  return json.loads(i.replace("'", '"'))
+
+def mining(block):
+  info = subprocess.check_output(['./lambda-cli.py','getmininginfo']).decode()
+  post(u"new block\n>>>\n"+info)
+  info = jsonloads(info)
+  with open('problems/{}.json'.format(block), 'w') as f:
+      json.dump(info, f)
+
+  puzzlef = 'problems/{}puzzle'.format(block)
+  with open(puzzlef, 'w') as f:
+      f.write(info["puzzle"])
+  taskf = 'problems/{}task'.format(block)
+  with open(taskf, 'w') as f:
+      f.write(info["task"])
+
+  post(u"start Solving")
+  puzzlesolf = 'problems/{}puzzle.sol'.format(block)
+  with open(puzzlesolf, "w") as o:
+      with open(puzzlef, "r") as i:
+          solveerr = subprocess.Popen('cat',stderr=subprocess.PIPE,stdin=i,stdout=o)
+          err = solveerr.communicate()[1].decode()
+          post(u"Solve Err: " + err)
+  with open(puzzlesolf, "r") as o:
+      post("Solve Result: "+ o.read())
+
+  post(u"End. start Generating")
+  taskdescf = 'problems/{}task.desc'.format(block)
+  with open(taskdescf, "w") as o:
+      with open(taskf, "r") as i:
+          generr = subprocess.Popen('cat',stderr=subprocess.PIPE,stdin=i,stdout=o)
+          err = generr.communicate()[1].decode()
+          post(u"Gen Err: " + err)
+  with open(taskdescf, "r") as o:
+      post("Gen Result: "+ o.read())
+
+  post(u"End. start Submit...")
+  sub = subprocess.check_output(['echo', './lambda-cli.py','submit',str(block),puzzlesolf,taskdescf]).decode()
+  post("Result: "+sub)
+
+def check_mining():
+  blockinfo = subprocess.check_output(['./lambda-cli.py','getblockchaininfo']).decode()
+  block = int(jsonloads(blockinfo)["block"])
+  block = int(jsonloads(blockinfo)["block"])
+  db = ETN()
+  if int(db.get("last_block")) != int(block):
+    print("Start mining")
+    mining(block)
+    db.set("last_block", int(block))
+
 def put_coininfo():
   blockinfo = subprocess.check_output(['./lambda-cli.py','getblockchaininfo']).decode()
   balance = subprocess.check_output(['./lambda-cli.py','getbalance']).decode()
@@ -54,17 +105,12 @@ def put_coininfo():
   )
   post(res)
 
-  block = int(json.loads(blockinfo.replace("'", '"'))["block"])
-  db = ETN()
-  if int(db.get("last_block")) != int(block):
-    db.set("last_block", int(block))
-    info = subprocess.check_output(['./lambda-cli.py','getmininginfo']).decode()
-    post(u"new block\n>>>\n"+info)
-
 import subprocess
+schedule.every(1).minutes.do(check_mining)
 schedule.every(5).minutes.do(put_coininfo)
 
 put_coininfo()
+check_mining()
 
 while True:
     schedule.run_pending()
